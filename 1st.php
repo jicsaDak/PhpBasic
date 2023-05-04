c46692b74402eb2caa5b346f6de5d1cc7d91ad82
unknown <j.i.c.sadak@gmail.com>
2023-05-04 22:49:29 +0600

    form ,session,if ans sested if

--- 1st.php
+++ 1st.php
@@ -1,30 +1,53 @@
 <?php
 
 echo " haw maw khaw!!" . "<br><br>";
+
+
 ?>
 
 <html>
-<?php
-if (isset($_GET['submit'])) {
-    $x = $_GET['num1'];
-    $y = $_GET['num2'];
-?>
-
-    <form method="GET">
+<body>
+<form  action="" method="get">
         1st num:
         <input type="number" id="num1" name="num1">
         2nd num:
         <input type="number" id="num2" name="num2">
+        3rd num:
+        <input type="number" id="num3" name="num3">
 
-        <input type="submit" name="submit" value="compare">
+        <input type="submit" name="submit" value="submit">
 
     </form>
-<?php
 
-    if ($x == $y) {
-        echo "they are equal";
-    } else {
-        echo "they are not equal";
+<?php 
+
+if (isset($_GET['submit'])) {
+    $x = $_GET['num1'];
+    $y = $_GET['num2'];
+    $z = $_GET['num3'];
+?>
+
+    
+<?php
+echo "$x*$y=".$x*$y."<br>";
+echo "$x+$y=".$x+$y."<br>";
+echo "$x-$y=".$x-$y."<br>";
+echo $z ."<br>";
+
+ if ($x%2==0)
+{ echo $x."is a even Number "."<br>" ;}
+else {echo $x." is aodd Number "."<br>"; }
+
+    if ($x == $y && $x==$z) {
+        echo "$x ans $y and $z "."they are equal";
+    } else if ($x>$y && $x>$z ) {
+        echo "$x ans $y"." Number1 in greater then Number2";
+    }
+    else if ($y>$x&& $y>$z) {
+        echo "$x ans $y and $z "." Number2 in greater then Number1";
+    }
+    else {
+        echo "$x ans $y and $z "." Number3 in greater then Number1";
     }
 }
 
@@ -32,5 +55,5 @@ if (isset($_GET['submit'])) {
 ?>
 
 
-
+</body>
 </html>
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Folder for store some basic Practices.
