000b779e802ebdf87f99f6225c7d11e2d43c0ff8
unknown <j.i.c.sadak@gmail.com>
2023-05-10 15:48:25 +0600

    simple Update

--- 3rdloopProblemSolving.php
+++ 3rdloopProblemSolving.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>3ProblemSolving</title>
 </head>
 <body><tr> <h1> 1.	Print N to 1</h1></tr>
 
@@ -221,29 +221,11 @@ else{
 </h3>
 <?php 
 
-
-
-
 }
 
 else{echo "Plse enter the values";}
 
-
-
-
-
-
 ?>
-
-
 <hr>
-
-
-
-
-
-
-
-
 </body>
 </html>
