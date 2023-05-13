2a7a7188fa567c9fb6616c4d7fb7b73aef0a9b26
unknown <j.i.c.sadak@gmail.com>
2023-05-13 21:16:02 +0600

    finally no 3 problem solved but have some worning

---
+++ 4thC_Preactice_PS_3.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4thC_Preactice_PS_3.php</title>
+</head>
+
+<body>
+
+
+
+
+    <?php
+
+    $stri = "hellow";
+    $n = strlen($stri);
+    $temp = 0;
+    $temp = array();
+
+    for ($i = 0; $i < $n; $i++) {
+
+        for ($j = 0; $j < $n; $j++) {
+            if ($stri[$i] === $stri[$j]) {
+
+                $temp[$i]++;
+               
+
+
+
+
+            }
+        }
+    }
+
+    for ($i = 0; $i < $n; $i++) {
+        $stri[$i];
+        $Dupli = false;
+    
+        
+        for ($j = 0; $j < $i; $j++) {
+            if ($stri[$j] === $stri[$i]) {
+                $Dupli = true;
+                break;
+            }
+        }
+    
+        
+        if (!$Dupli) {
+            echo $stri[$i] .":".$temp[$i]. "<br>";
+        }
+    }
+
+    ?>
+</body>
+
+</html>
